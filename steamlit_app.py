111d0fdd1b7221e751354c426e3b145e2adcfb73
ShlomiM1306 <131142407+ShlomiM1306@users.noreply.github.com>
2023-04-19 16:28:18 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -20,7 +20,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all")
+my_all_fruit_list = my_all_fruit_list.set_index('name')
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(my_all_fruit_list)
